eb9b4d396e3244db557213621922285e22e150f0
Dragan Hadzic <hadzic.dragan9@gmail.com>
2020-04-13 00:00:08 +0200

    Updated/Added scripts

--- runApp.sh
+++ runApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-application_name=Observer.jar
+source env_vars.sh
 
 delete_data_directories() {
 	echo "Deleting data/error directory"
@@ -18,7 +18,31 @@ delete_data_directories() {
 	rm -rf "data/"
 }
 
+set_pom_target() {
+if [[ $_env == "dev" ]]; then
+	echo "Removing prevoius ${pom} file"
+	rm $pom
+	echo "Using ${dev_pom} file"
+	cp $dev_pom pom.xml
+else
+	echo "Removing prevoius ${pom} file"
+	rm $pom
+	echo "Using ${prod_pom} file"
+	cp $prod_pom pom.xml
+fi
+}
+
+replace_properties_file() {
+	echo "Removing previous ${application_file_properties}"
+	rm $application_file_properties
+	echo "Using ${application_file_default_properties}"
+	cp ${application_file_default_properties} ${application_file_properties}
+}
+
 (delete_data_directories)
+
+(set_pom_target)
+(replace_properties_file)
 
 mvn clean && mvn package -DskipTests
 
